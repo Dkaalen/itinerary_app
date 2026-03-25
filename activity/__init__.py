92637383bf7cff293a9f0869add070240c987bff
Dkaalen <denniskaalen@gmail.com>
2026-03-25 23:57:07 +0100

    Update __init__.py

--- activity/__init__.py
+++ activity/__init__.py
@@ -1,21 +1,2 @@
-from .catalog import (
-    ACTIVITIES,
-    ACTIVITIES_BY_DESTINATION,
-    DESTINATIONS,
-    as_streamlit_options,
-    destination_options,
-    find_activity,
-    get_activities,
-)
-from .models import Activity
-
-__all__ = [
-    "ACTIVITIES",
-    "ACTIVITIES_BY_DESTINATION",
-    "DESTINATIONS",
-    "Activity",
-    "as_streamlit_options",
-    "destination_options",
-    "find_activity",
-    "get_activities",
-]
+from .catalog import get_activities
+from .catalog import destination_options
